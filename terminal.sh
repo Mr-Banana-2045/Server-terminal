bdafb247c6f4711ebbc9c639ce9b91a213dd0c25
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2024-04-02 15:13:10 +0330

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,10 +1,12 @@
 while sleep 0
 do
-read -p 'command-$ ' input
 command_not_found_handle() {
 echo -e $input'\033[91m : Error Command\033[00m'
 }
-if [ $input == $input ]; then
+read -p 'command-$ ' input
+if [ $input == 'dir' ]; then
+curl -s https://sheetdb.io/api/v1/9k4t6tfssbyud | mz= jq '.[]' | awk -F: '{print $1}' | sed 's/"//g' | sed 's/{//g' | sed 's/}//g'
+elif [ $input == $input ]; then
 curl -s https://sheetdb.io/api/v1/9k4t6tfssbyud | mz= jq '.[] | .'$input | sed 's/"//g' > mmmm.sh ; source mmmm.sh
 fi
-don
+done
